7c5dc425dbf075eef5bceb2362945926ba358749
Torrex1 <flendik666@gmail.com>
2024-10-28 20:09:14 +0300

    add sidebar

---
+++ src/components/SideNav.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="wrapper">
+        <h3>Your Friends</h3>
+        <!-- убрать в компонент {list} -->
+        <ul style="display: grid; grid-template-columns: 1fr; gap: 5px;">
+            <li>    
+                <UserSidebar />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    import UserSidebar from './UserSidebar.vue';
+</script>
+
+<style scoped>
+.wrapper {
+    position: fixed;
+    width: 220px;
+    height: 100vh;
+    background-color: #f3f3f3;
+    margin-top: 1px;
+    overflow-y: auto;
+    padding: 5px 0;
+}
+
+h3 {
+    margin: 5px 0 10px 10px;
+}  
+</style>
