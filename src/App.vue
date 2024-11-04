8450bee1ecf9ef8b296840fabda23fd6a9eb3cb0
Torrex1 <flendik666@gmail.com>
2024-11-04 22:45:51 +0300

    change layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
-import Header from './components/Header.vue';
+    import Header from './components/Header.vue';
+    import SideNav from './components/SideNav.vue';
+    import UserPopup from './components/UsersComponents/UserPopup.vue';
+    import PostContent from './components/PostComponents/PostContent.vue';
 </script>
 
 <template>
     <Header />
+    <!-- <UserPopup/> -->
+    <SideNav />
+
+    <main>
+        <PostContent />
+    </main>
+
 </template>
 
 <style scoped>
-
+    main {
+        margin-left: 250px;
+        margin-top: 20px;
+    }
 </style>
