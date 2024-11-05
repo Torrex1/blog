e05ef106611c558af1e84d0fa428660dd5669d1b
Torrex1 <flendik666@gmail.com>
2024-11-05 22:04:26 +0300

    add open popup logic

--- src/components/UsersComponents/UserIcon.vue
+++ src/components/UsersComponents/UserIcon.vue
@@ -1,8 +1,21 @@
 <template>
-    <div class="user-profile">
+    <div class="user-profile" @click="openModal">
         <img src="https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-70x70.png" alt="user">
     </div>
+
+    <UserPopup />
 </template>
+
+<script setup>
+    import { useModalStore } from '../../store/modalStore';
+
+    const modalStore = useModalStore();
+
+    const openModal = () => {
+        modalStore.openModal();    
+    }
+
+</script>
 
 <style scoped>
 .user-profile {
