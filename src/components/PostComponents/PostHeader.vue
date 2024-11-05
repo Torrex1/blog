6db28548dc4686ea1a12e9af3d42adcc28124044
Torrex1 <flendik666@gmail.com>
2024-11-05 19:38:03 +0300

    add new user icon

--- src/components/PostComponents/PostHeader.vue
+++ src/components/PostComponents/PostHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="post-header">
         <div class="user-info">
-            <img src="https://ui-avatars.com/api/?name=Вася&rounded=true&background=f8ebf4&color=d36ba9" alt="">
+            <img src="https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-70x70.png" alt="">
             <span class="username">Вася Пупкин</span>
         </div>
         <time>19:00</time>
