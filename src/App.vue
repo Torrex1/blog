5e208d7f2c11254f3b539cfc52a8350fbd45388d
Torrex1 <flendik666@gmail.com>
2024-11-07 22:07:15 +0300

    add home page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,3 @@
-<script setup>
-    import Header from './components/Header.vue';
-    import SideNav from './components/SideNav.vue';
-    import UserPopup from './components/UsersComponents/UserPopup.vue';
-    import PostContent from './components/PostComponents/PostContent.vue';
-</script>
-
 <template>
-    <Header />
-    <!-- <UserPopup/> -->
-    <SideNav />
-
-    <main>
-        <PostContent />
-    </main>
-
+    <RouterView />
 </template>
-
-<style scoped>
-    main {
-        margin-left: 250px;
-        margin-top: 20px;
-    }
-</style>
