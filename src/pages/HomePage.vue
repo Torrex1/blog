c26546fc8f5ae20dc510aa64dd2de84ba8bc64f0
Torrex1 <flendik666@gmail.com>
2024-11-07 22:06:53 +0300

    add home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import { useModalStore } from '../store/modalStore';
+    import SideNav from '../components/SideNav.vue';
+    import UserPopup from '../components/UsersComponents/UserPopup.vue';
+    import PostContent from '../components/PostComponents/PostContent.vue';
+    import Header from '../components/Header.vue';
+</script>
+
+<template>
+    <Header/>
+        
+    <UserPopup v-show="useModalStore().isModalOpen" />
+    <SideNav />
+
+    <main>
+        <PostContent />
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 25px;
+    
+    margin-left: 250px;
+    margin-top: 20px;
+}
+</style>
