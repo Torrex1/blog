6159d79e5653b7db644748b1be3cffeb2d2f060a
Torrex1 <flendik666@gmail.com>
2024-11-15 00:21:20 +0300

    refactoring login and register endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import User from "./NewUser.js";
 import cors from "cors";
+
+import * as UserController from "./controllers/UserController.js";
 
 const app = express();
 const port = 3000;
@@ -10,30 +11,9 @@ const DB_URL = "mongodb+srv://admin:wwwwww@cluster0.ur2f9.mongodb.net/";
 app.use(cors());
 app.use(express.json());
 
+app.post('/login', UserController.login);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
-app.post('/login', async (req, res) => {
-    const {username, password, email} = req.body;
-    const user = await User.findOne({ username, password });
-
-    if (!user) {
-        return res.status(404).json({message: "User not found"});
-    } else {
-        return res.status(200).json(user);
-    }
-})
-
-app.post('/register', async (req, res) => {
-    const {username, password, email} = req.body;
-    const user = new User({username, password, email});
-
-    await user.save();
-
-    res.json(user);
-})
+app.post('/register', UserController.register);
 
 async function start() {
     try {
@@ -49,3 +29,6 @@ async function start() {
 
 start();
 
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// });
